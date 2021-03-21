a7931954df1633e5a3a935ff0589006a1b35822c
jameschris854 <jameschris854@gmail.com>
2021-03-21 11:56:49 +0530

    second commit-multer

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,7 +2,51 @@ const AppError = require('../utils/appError');
 const User = require('./../models/userModel');
 const catchAsync = require('./../utils/catchAsync');
 const factory = require('./../controllers/handlerFactory');
+const multer = require('multer');
+const sharp = require('sharp');
 
+////Multer config
+// const multerStorage = multer.diskStorage({
+//   destination: (req, file, cb) => {
+//     cb(null, 'public/img/users');
+//   },
+//   filename: (req, file, cb) => {
+//     // user-75458735asd-3213323112312.jpg
+//     const ext = file.mimetype.split('/')[1];
+//     cb(null, `user-${(req.user, id)}-${Date.now()}.${ext}`);
+//   },
+// });
+
+const multerStorage = multer.memoryStorage();
+
+const multerFilter = (req, file, cb) => {
+  if (file.mimetype.startsWith('image')) {
+    cb(null, true);
+  } else {
+    cb(new AppError('Not an image!,please upload only images.', 400), false);
+  }
+};
+const upload = multer({
+  storage: multerStorage,
+  fileFilter: multerFilter,
+});
+
+exports.uploaduserPhoto = upload.single('photo');
+
+exports.resizeUserPhoto = (req, res, next) => {
+  if (!req.file) return next();
+
+  req.file.filename = `user-${(req.user, id)}-${Date.now()}.jpeg`
+
+  sharp(req.file.buffer)
+    .resize(500, 500)
+    .toFormat('jpeg')
+    .jpeg({ quality: 90 })
+    .toFile(`public/img/users/${req.file.filename}`)
+
+    next();
+};
+//User controller
 const filterObj = (obj, ...allowedFields) => {
   const newObj = {};
   Object.keys(obj).forEach((el) => {
@@ -11,7 +55,7 @@ const filterObj = (obj, ...allowedFields) => {
   return newObj;
 };
 
-exports.getAllUsers = factory.getAll(User)
+exports.getAllUsers = factory.getAll(User);
 
 exports.updateMe = catchAsync(async (req, res, next) => {
   //create error if user post password data
@@ -25,6 +69,7 @@ exports.updateMe = catchAsync(async (req, res, next) => {
   }
   //filter the protected fields such as admin and password
   const filteredBody = filterObj(req.body, 'name', 'email');
+  if (req.file) filteredBody.photo = req.file.filename;
   //Update user document
 
   const updatedUser = await User.findByIdAndUpdate(req.user.id, filteredBody, {
@@ -38,11 +83,11 @@ exports.updateMe = catchAsync(async (req, res, next) => {
     },
   });
 });
-exports.getMe = (req,res,next)=>{
+exports.getMe = (req, res, next) => {
   req.params.id = req.user.id;
   console.log(`check ${req.params.id}`);
-  next()
-}
+  next();
+};
 exports.deleteMe = catchAsync(async (req, res, next) => {
   await User.findByIdAndUpdate(req.user.id, { active: false });
 
@@ -52,8 +97,7 @@ exports.deleteMe = catchAsync(async (req, res, next) => {
   });
 });
 
-exports.getUser = factory.getOne(User)
-
+exports.getUser = factory.getOne(User);
 
 exports.createUser = (req, res) => {
   res.status(500).json({
